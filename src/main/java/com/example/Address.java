d8084bf20215ab32fa22fbb23969c4c30c9679d0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-10 10:39:00 +0100

    Implementing dependecy injection of Dependent object

---
+++ src/main/java/com/example/Address.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Address {
+    public String address;
+
+    public Address( String address) {
+        this.address = address;
+    }
+}
--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -2,4 +2,17 @@ package com.example;
 
 public class Employee {
 
+    //Dependency Injection
+    Address address;
+
+    Employee(Address address) {
+        this.address = address;
+    }
+
+    //Instance of Address class is provided by external source such as XML file either by constructor or setter method
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
 }
