a6f33d2e136fac272aed68c974ef3de9fbafa8b1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-11 01:55:22 +0100

    Implenting a singleton scoped bean which creates the same instance at all times

---
+++ src/main/java/com/singleton/MainApp.java
@@ -0,0 +1,16 @@
+package com.singleton;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        objA.setMessage("I'm Object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+        objB.getMessage();
+    }
+}
