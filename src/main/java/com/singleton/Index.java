d3098ef0d8a9d37487b80d3704cd6b80589b7c98
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-11 09:52:05 +0100

    Initialize and required getIndia bean

--- src/main/java/com/singleton/Index.java
+++ src/main/java/com/singleton/Index.java
@@ -7,7 +7,13 @@ public class Index {
     public static void main(String[] args) {
         AbstractApplicationContext context1 = new ClassPathXmlApplicationContext("lifecycle.xml");
         Greeting style = (Greeting) context1.getBean("greeting");
-        style.getMessage();
+        style.getMessage1();
+        style.getMessage2();
+
+        GreetingIndia styleIndia = (GreetingIndia) context1.getBean("helloIndia");
+        styleIndia.getMessage1();
+        styleIndia.getMessage2();
+        styleIndia.getMessage3();
         context1.registerShutdownHook();
     }
 }
